54d61e604ab68a5f28bba1e3ea2fca64b9c95da3
Nicolas Saumont <saumont.nicolas@gmail.com>
2023-11-29 21:42:58 +0100

    feat: router

---
+++ app/router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require('./controller/bookController');
+const authorController = require('./controller/authorController');
+const tagController = require('./controller/tagController');
+const userController = require('./controller/userController');
+const commentController = require('./controller/commentController');
+
+// Books url
+router.get('/books', bookController.getAllBooks);
+router.get('/books/:bookId', bookController.getOneBook);
+router.post('/books/:bookId', bookController.postOneBook);
+router.patch('/books/:bookId', bookController.editOneBook);
+router.delete('/books/:bookId', bookController.deleteOneBook);
+
+// Authors url
+router.get('/authors', authorController.getAllAuthors);
+router.get('/authors/:authorId', authorController.getOneAuthor);
+router.post('/authors/:authorId', authorController.postOneAuthor);
+router.patch('/authors/:authorId', authorController.editOneAuthor);
+router.delete('/authors/:authorId', authorController.deleteOneAuthor);
+router.get('/authors/:authorId/books', authorController.getBooksByAuthor);
+
+// Tags url
+router.get('/tags', tagController.getAllTags);
+router.post('/tags/:tagId', tagController.postOneTag);
+router.patch('/tags/:tagId', tagController.editOneTag);
+router.delete('/tags/:tagId', tagController.deleteOneTag);
+router.get('/tags/:tagId/books', tagController.getBooksByTag);
+
+// Users url
+router.post('/user', userController.postUser);
+router.get('/user/:userId', userController.getUser);
+router.patch('/user/:userId', userController.editUser);
+router.delete('/user/:userId', userController.deleteUser);
+
+module.exports = router;
